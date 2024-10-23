efe303fb4f18be715584a69694b3581fa4a2493b
jonulak <jonulak@gmail.com>
2024-10-22 23:38:47 -0400

    Add Unit Tests for Text Extraction Functions

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import io
+import pytest
+import os
+
+TEST_RESOURCE_DIR = f"{os.path.dirname(__file__)}/resources"
+
+
+def load_resource_file(file_name):
+    file = open(file_name, "rb")
+    data = io.BytesIO(file.read())
+    file.close()
+    return data
+
+
+@pytest.fixture(scope="module")
+def file_resources():
+    library = {}
+    for filename in os.listdir(TEST_RESOURCE_DIR):
+        library[filename.split(".")[0]] = load_resource_file(f"{TEST_RESOURCE_DIR}/{filename}")
+    yield library
